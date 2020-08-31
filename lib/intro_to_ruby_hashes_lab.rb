5212ce86f73f1bf86eae7e491a8ca7739dfd661e
Tyler Wallace <tywallace2017@gmail.com>
2020-08-31 16:51:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -5,7 +5,8 @@ end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
-   if hash[key] = hash
+   if hash[key] += hash[key] + 1 
+   else hash
 end
 
 def update_counting_hash(hash, key)
