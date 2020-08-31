b0636a1c45a7bff60a06282958638cd40f0407f0
Tyler Wallace <tywallace2017@gmail.com>
2020-08-31 16:59:54 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -6,7 +6,7 @@ end
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
    if hash[key]
-     hash[key] += 1 
+     hash[key] = hash[key] += 1 
    else 
      hash[key] = 1 
    end
